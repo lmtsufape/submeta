0814ec707f5c97772f74d2b5c4b9f853511ff4be
carlos <carlos@debian.carlos>
2020-05-28 15:14:41 -0300

    criando usuario como adm

--- resources/views/administrador/index.blade.php
+++ resources/views/administrador/index.blade.php
@@ -19,7 +19,7 @@
 	      </div>
 
 	      <div class="col-sm-4 d-flex justify-content-center">
-	         <a href="{{ route('admin.naturezas') }}" style="text-decoration:none; color: inherit;">
+	         <a href="{{ route('grandearea.index') }}" style="text-decoration:none; color: inherit;">
 	            <div class="card text-center " style="border-radius: 30px; width: 18rem;">
 	             <div class="card-body d-flex justify-content-center">
 	                  <h2 style="padding-top:15px">Natureza</h2>
