93cc9e74b577a79c7358766d7957553492345736
alinetenorio <aline.tenorio96@gmail.com>
2020-06-06 00:30:44 -0300

    listagem de editais - perfil participante

--- resources/views/administrador/editais.blade.php
+++ resources/views/administrador/editais.blade.php
@@ -132,6 +132,61 @@
       </tbody>
     </table>
   @endif
+
+  @if(auth()->user()->tipo === "participante")
+    <table class="table table-bordered">
+      <thead>
+        <tr>   
+          <th scope="col">Nome do Edital</th>
+          <th scope="col">Status</th>
+          <th scope="col">Data de Criação</th>
+          <th scope="col">Baixar edital</th>
+          <th scope="col">Opção</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($eventos as $evento)
+          <tr>
+            <td>
+              <a href="{{  route('evento.visualizar',['id'=>$evento->id])  }}" class="visualizarEvento">
+                  {{ $evento->nome }}
+              </a>
+            </td>
+            <td></td>
+            <td>{{ $evento->created_at }}</td>
+            <td style="text-align: center">
+              <a href="{{ route('baixar.edital', ['id' => $evento->id]) }}">
+                <img src="{{asset('img/icons/file-download-solid.svg')}}" width="15px">
+              </a>
+            </td>
+            <td>
+              <div class="btn-group dropright dropdown-options">
+                  <a id="options" class="dropdown-toggle " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      {{-- <img src="{{asset('img/icons/ellipsis-v-solid.svg')}}" style="width:8px"> --}}
+                  </a>
+                  <div class="dropdown-menu">
+                      <a href="{{ route('projetos.edital', ['id' => $evento->id]) }}" class="dropdown-item" style="text-align: center">
+                        Projetos submetidos
+                      </a>
+                      <a href="" class="dropdown-item" style="text-align: center">
+                        Visualizar resultado
+                      </a>
+                      {{-- 
+                      <a href="" class="dropdown-item" style="text-align: center">
+                        Resultado preeliminar
+                      </a>
+                      <a href="" class="dropdown-item" style="text-align: center">
+                        Resultado final
+                      </a> --}}
+                  </div>
+              </div>
+            </td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  @endif
+
 </div>
 
 @endsection
