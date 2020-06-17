5079b96d78bc5f0b63ab60dc06ce4dab1f319b04
carlos <carlos@debian.carlos>
2020-06-17 17:59:30 -0300

    ajuste na home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,10 +25,10 @@
       <div class="col-sm-6" style=" position: relative; top: 50px; padding: 25px;">
           <h4 style="color:  rgb(0, 140, 255);">Editais</h4>
           <div id="editais">
-                <ul class="list-editais flexcroll" style="list-style-type: none;">
+                <ul class="col-sm-12 list-editais flexcroll" style="list-style-type: none;">
                 @foreach ($eventos as $evento)
                     @if (\Carbon\Carbon::create($evento->fimSubmissao) > \Carbon\Carbon::create($hoje))
-                        <li class="li-editais">
+                        <li class="col-sm-12 li-editais">
                             <div class="container">
                                 <div class="row">
                                     <div class="col-sm-1">
@@ -40,7 +40,7 @@
                                     </div>
                                     <div class="col-sm-3">
                                         <a href="{{ route('evento.visualizarNaoLogado', ['id' => $evento->id]) }}">
-                                            <button class="btn btn-opcoes-edital" style="float: right;">
+                                            <button class="btn btn-opcoes-edital" style="float: left;">
                                                 Visualizar
                                             </button>
                                         </a>
