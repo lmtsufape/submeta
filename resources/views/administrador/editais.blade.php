f9faab3f371e020326b486f34652bb76165f8810
Gabriel-31415 <gabriel.uag.ufrpe@gmail.com>
2020-06-23 19:16:20 +0000

    modal para apagar projeto e edital

--- resources/views/administrador/editais.blade.php
+++ resources/views/administrador/editais.blade.php
@@ -50,15 +50,13 @@
                           
                           Visualizar Pareceres
                       </a>
-                      <form method="POST" action="{{route('evento.deletar',$evento->id)}}" class="text-center">
-                          {{ csrf_field() }}
-                          {{ method_field('DELETE') }}
-                          <button type="submit" class="dropdown-item">
-                              
-                              Deletar
+                        <!-- Button trigger modal -->
+                          <button type="button" class="dropdown-item text-center"  data-toggle="modal" data-target="#exampleModal">
+                            Deletar
                           </button>
 
-                      </form>
+                          
+                      
                   </div>
               </div>
               @endif
@@ -69,6 +67,34 @@
     </table> 
 
 </div>
+<!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Deletar edital</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>Você tem certeza que deseja deletar o edital:{{ $evento->nome }}?</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+          <form method="POST" action="{{route('evento.deletar',$evento->id)}}" class="text-center">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <button type="submit" class="btn btn-primary">
+                
+                Deletar
+            </button>
+
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 
 @endsection
 
