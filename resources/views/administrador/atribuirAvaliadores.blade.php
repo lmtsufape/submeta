49d31136250822f31dbcf4d3e214b327518cfa1c
Carlos André <carlos.andre1270@gmail.com>
2021-05-07 17:33:22 -0300

    view atribuir avaliadores

--- resources/views/administrador/atribuirAvaliadores.blade.php
+++ resources/views/administrador/atribuirAvaliadores.blade.php
@@ -4,37 +4,69 @@
 
 <div class="container">
 
-    <h2 style="margin-top: 100px; ">{{ Auth()->user()->name }}</h2>
-    <div class="container" >
-		<div class="row justify-content-center d-flex align-items-center" >
-		  
-		    <h3>Edital Selecionado: {{ $evento->nome }} </h3> 
-
-		</div>
+    <div class="row justify-content-center titulo-menu">
+		<h4>Edital Selecionado: {{ $evento->nome }} </h4>
 	</div>
 
        <div class="row justify-content-center d-flex align-items-center">
 	      <div class="col-sm-3 d-flex justify-content-center ">
 	         <a href="{{route('admin.selecionar', ['evento_id' => $evento->id])}}" style="text-decoration:none; color: inherit;">
-	            <div class="card text-center " style="border-radius: 30px; width: 13rem;height: 15rem;">
-	                  <div class="card-body d-flex justify-content-center">
-	                      <h2 style="padding-top:15px">Selecionar Avaliadores</h2>
-	                  </div>
-	                 
+	            <div class="card text-center card-menu">
+					<div class="card-body d-flex justify-content-center">
+						<div class="container">
+							<div class="row titulo-card-menu">
+								<div class="col-md-12">
+									<h2 style="padding-top:15px">Selecionar avaliadores</h2>
+								</div>
+							</div>
+							@php
+								$avaliadores = \App\Avaliador::count();
+							@endphp
+							<div class="row">
+								<div class="col-md-12">
+									<h5>Nº total de avaliadores:</h5>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-12">
+									<h1 class="quant-titulo-card">{{$avaliadores}}</h1>
+								</div>
+							</div>
+						</div>
+					</div>
 	            </div>
 	         </a>
 	      </div>
 
 	      <div class="col-sm-3 d-flex justify-content-center">
 	         <a href="{{ route('admin.projetos', ['evento_id' => $evento->id]) }}" style="text-decoration:none; color: inherit;">
-	            <div class="card text-center " style="border-radius: 30px; width: 13rem;height: 15rem;">
-	             <div class="card-body d-flex justify-content-center">
-	                  <h2 style="padding-top:15px">Selecionar Projetos</h2>
-	               </div>
+	            <div class="card text-center card-menu">
+					<div class="card-body d-flex justify-content-center">
+						<div class="container">
+							<div class="row titulo-card-menu">
+								<div class="col-md-12">
+									<h2 style="padding-top:15px">Selecionar projetos</h2>
+								</div>
+							</div>
+							@php
+								$projetos = \App\Trabalho::count();
+							@endphp
+							<div class="row">
+								<div class="col-md-12">
+									<h5>Nº total de projetos:</h5>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-12">
+									<h1 class="quant-titulo-card">{{$projetos}}</h1>
+								</div>
+							</div>
+						</div>
+					</div>
 	            </div>
 	         </a>
 	      </div>
-	      <div class="col-sm-3 d-flex justify-content-center">
+	      {{-- <div class="col-sm-3 d-flex justify-content-center">
 	         <a href="#" style="text-decoration:none; color: inherit;">
 	            <div class="card text-center " style="border-radius: 30px; width: 13rem;height: 15rem;">
 	             <div class="card-body d-flex justify-content-center">
@@ -42,7 +74,7 @@
 	               </div>
 	            </div>
 	         </a>
-	      </div>
+	      </div> --}}
 	   </div>
 
 
