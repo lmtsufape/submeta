3b5474445662493188a25295966104d57a0b0de8
Carlos André <carlos.andre1270@gmail.com>
2021-05-27 08:53:58 -0300

    view login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,24 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container content">
+<div class="container content" style="margin-top: 40px;">
     <div class="row justify-content-center">
         <div class="col-md-5">
             <div class="card card-login-cadastro">
-                <div class="card-header">{{ __('Login') }}</div>
+                {{-- <div class="card-header">{{ __('Login') }}</div> --}}
 
-                <div class="card-body">
+                <div class="card-body" style="font-weight: bolder;">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="row justify-content-center">
-                            <div class="titulo-login-cadastro">LOGIN</div>
+                            <div class="titulo-login-cadastro" style="margin-top: 20px; margin-bottom: 20px; font-size: 20px; color: rgb(0, 140, 255);">{{ __('Login') }}</div>
                         </div>
 
                         <div class="form-group row">
                             
                             <div class="col-md-12">
-                                <label for="email" class="col-form-label text-md-right">{{ __('Endereço de E-mail') }}</label>
+                                <label for="email" class="col-form-label text-md-right" style="color: rgb(0, 140, 255);">{{ __('Endereço de E-mail') }}</label>
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -32,7 +32,7 @@
                         <div class="form-group row">
                             
                             <div class="col-md-12">
-                                <label for="password" class="col-form-label text-md-right">{{ __('Senha') }}</label>
+                                <label for="password" class="col-form-label text-md-right" style="color: rgb(0, 140, 255);">{{ __('Senha') }}</label>
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
@@ -58,7 +58,7 @@
                         <div class="form-group row mb-0">
                             
                             <div class="col-md-12">
-                                <button type="submit" class="btn btn-primary" style="width:100%">
+                                <button type="submit" class="btn btn-primary" style="width:100%; font-weight: 700;">
                                     {{ __('Login') }}
                                 </button>
                                 <div class="row justify-content-center">
