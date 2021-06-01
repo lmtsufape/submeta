a33eecb457a439f5fa0d9e2337f2195d60d943d3
Gabriel-31415 <gabriel.uag.ufrpe@gmail.com>
2021-06-01 02:05:02 +0000

    ajustes

--- resources/views/componentes/input.blade.php
+++ resources/views/componentes/input.blade.php
@@ -3,7 +3,7 @@
 @endphp
 <div class="form-group">
   <label class=" control-label {{ $class }}" for="firstname">{{ $label }}<span style="color: red; font-weight:bold">*</span></label>
-  <div class="">
+  <div class="col-sm-5">
     {{ $slot }}
   </div>
 </div>
