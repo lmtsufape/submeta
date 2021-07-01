903599cbbadc5305d5a2b1ea5b1741a98884dc8b
Gabriel-31415 <gabriel.uag.ufrpe@gmail.com>
2021-07-01 16:09:32 +0000

    add consu opcional no edital, ajuste no complemento

--- resources/views/projeto/editar.blade.php
+++ resources/views/projeto/editar.blade.php
@@ -161,6 +161,18 @@
       
     }
   }
+  var SPMaskBehavior = function (val) {
+      return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+  },
+  spOptions = {
+      onKeyPress: function(val, e, field, options) {
+      field.mask(SPMaskBehavior.apply({}, arguments), options);
+      }
+  };
+
+  $("input.cpf:text").mask("000.000.000-00");
+  $("input.celular:text").mask(SPMaskBehavior, spOptions);
+  $("input.cep:text").mask("00000-000");
 
   buttonMais.addEventListener("click", (e) => {
     
