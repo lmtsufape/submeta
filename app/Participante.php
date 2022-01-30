db3a2c99afd25285b8de313ccdbcf71cc39adf53
Guilherme Silva <guilherme.dna06@gmail.com>
2022-01-30 15:32:26 -0300

    Adicionado tipo de bolsa ao discente

--- app/Participante.php
+++ app/Participante.php
@@ -10,7 +10,9 @@ class Participante extends Model
     use SoftDeletes;
     public const ENUM_TURNO = ['Matutino',  'Vespertino', 'Noturno', 'Integral'];
 
-	protected $fillable = ['rg', 'data_de_nascimento', 'curso', 'participante_id', 'turno', 'ordem_prioridade', 'periodo_atual', 'total_periodos', 'media_do_curso', 'linkLattes'];
+	protected $fillable = ['rg', 'data_de_nascimento', 'curso', 'participante_id', 'turno',
+        'ordem_prioridade', 'periodo_atual', 'total_periodos', 'media_do_curso', 'linkLattes',
+        'tipoBolsa'];
 
     public function user(){
         return $this->belongsTo('App\User');
---
+++ database/migrations/2021_11_16_101654_alter_table_participantes3.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableParticipantes3 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('participantes', function (Blueprint $table) {
+            $table->string('tipoBolsa')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('participantes', function (Blueprint $table) {
+            $table->dropColumn('tipoBolsa');
+        });
+    }
+}
