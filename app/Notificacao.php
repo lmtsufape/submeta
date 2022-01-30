545187d18fdc3fda284d9d6b26adb77a83bd6bfe
Guilherme Silva <guilherme.dna06@gmail.com>
2022-01-30 17:42:57 -0300

    Adicionado notificação ao sistema

---
+++ app/Notificacao.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Notificacao extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'lido', 'tipo', 'destinatario_id', 'remetente_id', 'perfil_id', 'trabalho_id',
+    ];
+
+    public function destinatario(){
+        return $this->belongsTo(User::class,'destinatario_id','id');
+    }
+
+    public function remetente(){
+        return $this->belongsTo(User::class,'remetente_id','id');
+    }
+
+    public function trabalho(){
+        return $this->belongsTo(Trabalho::class,'trabalho_id','id');
+    }
+}
