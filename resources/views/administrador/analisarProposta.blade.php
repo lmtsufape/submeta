f845e1c012adc1c28826f2897b71ac4fa062a205
Guilherme Silva <guilherme.dna06@gmail.com>
2022-02-01 10:33:39 -0300

    Adicionado relatórios na visualização de projeto

--- resources/views/administrador/analisarProposta.blade.php
+++ resources/views/administrador/analisarProposta.blade.php
@@ -276,6 +276,54 @@
         </div>
     </div>
 
+    <!--Relatórios-->
+    <div class="row justify-content-center" style="margin-top: 20px;">
+        <div class="col-md-10">
+            <div class="card" style="border-radius: 5px">
+                <div class="card-body" style="padding-top: 0.2rem;">
+                    <div class="container">
+                        <div class="form-row mt-3">
+                            <div class="col-sm-9"><h5 style="color: #234B8B; font-weight: bold">Relatórios</h5></div>
+                            <div class="col-sm-3 text-sm-right" >
+                                <a href="{{route('planos.listar', ['id' => $trabalho->id])}}"  class="button">Lista de Relatórios</a>
+                            </div>
+                        </div>
+                        <hr style="border-top: 1px solid#1492E6">
+
+                        <div class="row justify-content-center">
+                            {{-- Relatório Parcial  --}}
+                            <div class="col-sm-3">
+                                <label for="dt_inicioRelatorioParcial" class="col-form-label" style="font-weight: bold">{{ __('inicio do Relatório Parcial: ') }}</label>
+                            </div>
+                            <div class="col-sm-3">
+                                <input id="dt_inicioRelatorioParcial{{$evento->id}}" type="date" class="form-control" name="dt_inicioRelatorioParcial" value="{{$evento->dt_inicioRelatorioParcial}}" required autocomplete="dt_inicioRelatorioParcial" disabled autofocus>
+                            </div>
+                            <div class="col-sm-3">
+                                <label for="dt_fimRelatorioParcial" class="col-form-label" style="font-weight: bold">{{ __('Fim do Relatório Parcial: ') }}</label>
+                            </div>
+                            <div class="col-sm-3">
+                                <input id="dt_fimRelatorioParcial{{$evento->id}}" type="date" class="form-control" name="dt_fimRelatorioParcial" value="{{$evento->dt_fimRelatorioParcial}}" required autocomplete="dt_fimRelatorioParcial" disabled autofocus>
+                            </div>
+                            {{-- Relatório Final --}}
+                            <div class="col-sm-3">
+                                <label for="dt_inicioRelatorioFinal" class="col-form-label" style="font-weight: bold">{{ __('Início do Relatório Final:') }}</label>
+                            </div>
+                            <div class="col-sm-3">
+                                <input id="dt_inicioRelatorioFinal{{$evento->id}}" type="date" class="form-control" name="dt_inicioRelatorioFinal" value="{{$evento->dt_inicioRelatorioFinal}}" required autocomplete="dt_inicioRelatorioFinal" disabled autofocus>
+                            </div>
+                            <div class="col-sm-3">
+                                <label for="dt_fimRelatorioFinal" class="col-form-label" style="font-weight: bold">{{ __('Fim do Relatório Final:') }}</label>
+                            </div>
+                            <div class="col-sm-3">
+                                <input id="dt_fimRelatorioFinal{{$evento->id}}" type="date" class="form-control" name="dt_fimRelatorioFinal" value="{{$evento->dt_fimRelatorioFinal}}" required autocomplete="dt_fimRelatorioFinal" disabled autofocus>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <!--Avaliadores-->
     <div class="row justify-content-center" style="margin-top: 20px;">
         <div class="col-md-10">
