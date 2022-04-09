39586201c227739cf5e9f55e2b404a6cad64dcf4
Guilherme Silva <guilherme.dna06@gmail.com>
2022-04-09 10:17:04 -0300

    Alterado maneira visual de como é definida a bolsa do participante

--- resources/views/administrador/resultadosProjetos.blade.php
+++ resources/views/administrador/resultadosProjetos.blade.php
@@ -2,7 +2,13 @@
 
 @section('content')
 <div class="container" style="margin-top: 100px;">
-        <div class="row justify-content-center">
+    @if (session('sucesso'))
+        <div class="alert alert-success" role="alert">
+            {{ session('sucesso') }}
+        </div>
+    @endif
+
+    <div class="row justify-content-center">
             <div class="col-sm-12">
                 <div class="row" >
                     <div class="col-sm-4">
@@ -33,6 +39,7 @@
             <table class="table table-bordered" style="display: block; white-space: nowrap; border-radius:10px; margin-bottom:0px">
                 <thead>
                 <tr>
+                    <th scope="col">Posição</th>
                     <th scope="col">Pontuação</th>
                     <th scope="col" style="width: 100%;">Nome do projeto</th>
                     <th scope="col">Proponente</th>
@@ -44,9 +51,11 @@
                 </tr>
                 </thead>
                 <tbody id="projetos">
+                @php $cont=1;@endphp
                     @foreach($trabalhos as $trabalho)
                         @if($trabalho->status == 'aprovado')
                         <tr>
+                            <td>{{$cont}}</td>
                             <td>{{$trabalho->pontuacao}}</td>
                             <td style="max-width:100px; overflow-x:hidden; text-overflow:ellipsis">
                                 {{$trabalho->titulo}}
@@ -90,26 +99,19 @@
                                 </button>
                             </td>
                         </tr>
+                        @php $cont+=1;@endphp
                         @endif
                     @endforeach
                 </tbody>
             </table>
             </div>
         </div>
-    <div class="row justify-content-center" >
-        <div class="col-md-12">
-            <br>
-            {{ $trabalhos->links() }}
-
-        </div>
-
-    </div>
 </div>
 {{--Janelas--}}
 @foreach($trabalhos as $trabalho)
     <div class="modal fade" id="modalConfirmTrab{{$trabalho->id}}" tabindex="-1" role="dialog"
          aria-labelledby="modalConfirmLabel" aria-hidden="true">
-        <div class="modal-dialog modal-md modal-dialog-centered" role="document">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h4 class="modal-title" id="modalConfirmLabel" align="center" title="Participantes do {{$trabalho->titulo}}">
@@ -121,19 +123,18 @@
                 <div class="modal-body">
                     @foreach($trabalho->participantes as $participante)
                         <div class="row modal-header-submeta">
-                            <div class="col-sm-8">
+                            <div class="col-sm-7" style="padding-left: 0px">
                                 <p style="font-size: 22px">Discente: {{$participante->user->name}}</p>
                             </div>
-                            <div class="col-sm-4" align="left" style="padding-left: 0px">
-                                <button type="button"  class="btn btn-primary" data-dismiss="modal" data-toggle="modal" data-target="#modalConfirm{{$participante->id}}" onclick="myFunction({{$trabalho->id}})">
-                                    @if($participante->tipoBolsa==null)
-                                        Não Definida
-                                    @elseif($participante->tipoBolsa == "Voluntario")
-                                        Voluntário
-                                    @else
-                                        {{$participante->tipoBolsa}}
-                                    @endif
-                                </button>
+                            <div class="col-sm-5 text-right" style="padding-right: 0px">
+                                <form id="alteracao_bolsa{{$participante->id}}" method="POST" action="{{route('bolsa.alterar')}}">
+                                    @csrf
+                                    <input type="hidden" id="participante_{{$participante->id}}" name="id" value="{{$participante->id}}">
+                                    <input type="radio" id="bolsista{{$participante->id}}" name="tipo" value="Bolsista" required @if($participante->tipoBolsa=='Bolsista') checked @endif> Bolsista
+                                    <input type="radio" id="voluntario{{$participante->id}}" name="tipo" value="Voluntario" required @if($participante->tipoBolsa=='Voluntario') checked @endif> Voluntario
+
+                                    <button type="submit" class="btn btn-primary" form="alteracao_bolsa{{$participante->id}}">Definir</button>
+                                </form>
                             </div>
                         </div>
                         <br>
@@ -142,60 +143,6 @@
             </div>
         </div>
     </div>
-
-    @foreach($trabalho->participantes as $participante)
-        {{-- Janela de alocação de bolsa--}}
-        <div class="modal fade" id="modalConfirm{{$participante->id}}" tabindex="-1" role="dialog"
-             aria-labelledby="modalConfirmLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title" id="modalConfirmLabel" align="center">
-                            Confirmar alteração do tipo de bolsa ?</h4>
-                    </div>
-                    @if($participante->tipoBolsa!=null)
-                        <div class="modal-body">
-                            <h5 class="modal-title" id="modalConfirmLabel" align="center">
-                                @if($participante->tipoBolsa=='Voluntario')
-                                    O discente {{$participante->user->name}} será definido como bolsista
-                                @else
-                                    O discente {{$participante->user->name}} será definido como voluntário
-                                @endif
-                            </h5>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-danger" data-dismiss="modal">
-                                Não
-                            </button>
-                            <a type="button" href="{{ route('bolsa.alterar',['id'=>$participante->id, 'tipo'=>1])  }}"  id="btnSubmit" class="btn btn-info">
-                                Sim
-                            </a>
-                        </div>
-                    @else
-                        <div class="modal-body">
-                            <div class="row">
-                                <div class="col-6">
-                                    <a  style="float: right;" type="button" href="{{ route('bolsa.alterar',['id'=>$participante->id, 'tipo'=>1])  }}"  id="btnSubmit" class="btn btn-info">
-                                        Voluntário
-                                    </a>
-                                </div>
-                                <div class="col-6">
-                                    <a style="float: left;" type="button" href="{{ route('bolsa.alterar',['id'=>$participante->id, 'tipo'=>2])  }}"  id="btnSubmit" class="btn btn-info">
-                                        Bolsista
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-danger" data-dismiss="modal">
-                                Cancelar
-                            </button>
-                        </div>
-                    @endif
-                </div>
-            </div>
-        </div>
-    @endforeach
 @endforeach
 
 @endsection
@@ -204,7 +151,7 @@
 <script>
     function buscarProjeto(input) {
         var projetos = document.getElementById('projetos').children;
-        if(input.value.length > 2) {      
+        if(input.value.length > 2) {
             for(var i = 0; i < projetos.length; i++) {
                 var nomeProjeto = projetos[i].innerText;
                 if(nomeProjeto.substr(0).indexOf(input.value) >= 0) {
