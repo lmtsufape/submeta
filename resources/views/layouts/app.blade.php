1f4a1d73b29fa352c08eecda26129afbb7fbbf17
Guilherme Silva <guilherme.dna06@gmail.com>
2022-04-19 23:26:38 -0300

    Retirada de footer no momento de imprimir a página de resultados

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -272,7 +272,7 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
-    <div class="styleRodape" style="background-color:#fff">
+    <div class="styleRodape noPrint" style="background-color:#fff">
       <div class="container">
           <div class="form-row justify-content-center">
               <div class="col-sm-4" style="margin-top: 2.5rem;">
@@ -320,5 +320,15 @@
           });
       @endif
   </script>
+    <style>
+        @media print {
+            .noPrint{
+                display:none;
+            }
+            .doPrint{
+                display:block !important;
+            }
+        }
+    </style>
 </body>
 </html>
