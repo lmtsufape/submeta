9903da630dcb174faec8b0871589f7888a6f46fd
Guilherme Silva <guilherme.dna06@gmail.com>
2022-04-19 23:19:08 -0300

    Adicionado exibição de modalidade caso a mesma exista

--- resources/views/administrador/analisarProposta.blade.php
+++ resources/views/administrador/analisarProposta.blade.php
@@ -67,6 +67,7 @@
                                 @endif
                             </div>
 
+                            @if($evento->tipo != "PIBEX")
                             <div class="col-md-12">
                                 <br>
                                 <b style="color: #4D4D4D;">Grupo de Pesquisa: </b>
@@ -80,6 +81,14 @@
                                 <b style="color: #4D4D4D;">Valor da Planilha de Pontuação: </b>
                                 <a style="color: #4D4D4D;">{{$trabalho->pontuacaoPlanilha}}</a>
                             </div>
+                            @endif
+                            @if($trabalho->modalidade != null)
+                                <div class="col-md-12">
+                                    <br>
+                                    <b style="color: #4D4D4D;">Modalidade: </b>
+                                    <a style="color: #4D4D4D;">{{$trabalho->modalidade}}</a>
+                                </div>
+                            @endif
                         </div>
                     </div>
                 </div>
@@ -130,7 +139,7 @@
                                             <div class="modal-header" style="overflow-x:auto; padding-left: 31px">
                                                 <h5 class="modal-title" id="exampleModalLabel" style= "color:#1492E6">Informações Participante</h5>
 
-                                                <button type="button" class="close" aria-label="Close" style="padding-top: 8px; color:#1492E6" onclick="abrirHistorico({{$participante->id}}, 0)">
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close" style="padding-top: 8px; color:#1492E6">
                                                     <span aria-hidden="true">&times;</span>
                                                 </button>
                                             </div>
