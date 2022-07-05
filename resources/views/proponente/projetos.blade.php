86b9c319184cb5d18f88e4d1336e0e09c6080b50
unknown <antoniodurvalc@gmail.com>
2022-07-05 09:40:44 -0300

    Permissao de edicao(proponente) ate o inicio do projeto

--- resources/views/proponente/projetos.blade.php
+++ resources/views/proponente/projetos.blade.php
@@ -87,12 +87,13 @@
                                 <img src="{{asset('img/icons/ellipsis-v-solid.svg')}}" style="width:8px">
                             </a>
                             <div class="dropdown-menu">
-
-                                    <a href="{{ route('trabalho.editar', ['id' => $projeto->id]) }}" class="dropdown-item" style="text-align: center;">
-                                        Editar
-                                    </a>
-                                    <hr class="dropdown-hr">
-
+                              
+                              @if($hoje <= $projeto->evento->inicioProjeto)
+                                <a href="{{ route('trabalho.editar', ['id' => $projeto->id]) }}" class="dropdown-item" style="text-align: center;">
+                                    Editar
+                                </a>
+                                <hr class="dropdown-hr">
+                              @endif
 
                                 @if( $projeto->status== 'aprovado')
                                     <a href="{{route('trabalho.trocaParticipante', ['evento_id' => $projeto->evento->id, 'projeto_id' => $projeto->id])}}" class="dropdown-item" style="text-align: center;">
