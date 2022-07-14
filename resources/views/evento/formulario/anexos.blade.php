67c9031e6c8a69099c96a637be2e072ba9a2da97
GuilhermeGz <guilherme.dna06@gmail.com>
2022-07-14 07:10:17 -0300

    Adicionado campo para recebimento de documento extra nas telas de propostas

--- resources/views/evento/formulario/anexos.blade.php
+++ resources/views/evento/formulario/anexos.blade.php
@@ -115,6 +115,17 @@
               </div>
             </div>
           @endif
+          @if($edital->nome_docExtra != null)
+            <div class="form-group col-md-6">
+              <label title="{{$edital->nome_docExtra}}" for="anexo_docExtra" class="col-form-label font-tam" style="font-weight: bold">{{$edital->nome_docExtra}}: @if($edital->obrigatoriedade_docExtra == true) <span style="color: red; font-weight:bold">*</span> @endif</label>
+              <input type="file" class="input-group-text" name="anexo_docExtra"  accept=".pdf,.docx,.doc,.zip " @if($edital->obrigatoriedade_docExtra == true) required @endif/>
+              @error('anexo_docExtra')
+              <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
+                    <strong>{{ $message }}</strong>
+                  </span>
+              @enderror
+            </div>
+          @endif
         </div>
       </div>
     </div>
