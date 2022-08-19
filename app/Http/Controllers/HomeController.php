afa12af22c55f834bf1f3a2a14e42acc599c5c97
Guilherme Silva <guilherme.dna06@gmail.com>
2022-08-19 16:57:53 -0300

    Ajuste em acesso de avaliador

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -46,7 +46,6 @@ class HomeController extends Controller
           }
           else if (Auth::user()->avaliadors != null) {
             $flagAvalRelatorio = count(AvaliacaoRelatorio::where('user_id',Auth::user()->id )->get());
-            dd($flagAvalRelatorio);
             return view('avaliador.index', compact('flagAvalRelatorio'));
           }
           else if (Auth::user()->participantes != null) {
