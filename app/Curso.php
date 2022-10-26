2c8e7fe1dcc9b9b8b6b6a10272671f85fe93e12a
S-Nathalia <nathysantos1516@gmail.com>
2022-10-25 21:28:31 -0300

    criacao da relacao entre proponente de cursos

--- app/Curso.php
+++ app/Curso.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Curso extends Model
 {
-    //
+    protected $fillable = [
+        'nome',
+    ];
+
+    public function proponentes()
+    {
+        return $this->belongsToMany('App\Proponente', 'proponentes_cursos', 'curso_id');
+    }
 }
