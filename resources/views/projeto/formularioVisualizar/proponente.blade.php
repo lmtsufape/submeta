2d041c3b162be51e7771d99dbdcc033c57622619
Lucas Henrique <lucasmonte2011@gmail.com>
2022-11-07 20:23:53 -0300

    Adicionando campo Conflitos de Interesse ao visualizar um projeto

--- resources/views/projeto/formularioVisualizar/proponente.blade.php
+++ resources/views/projeto/formularioVisualizar/proponente.blade.php
@@ -1,58 +1,82 @@
 <!-- Proponente -->
-<div class="col-md-10" style="text-align: center; margin-top:2rem"><h4 style="margin-top: 1rem;">2º Passo</h4></div>
-<div class="col-md-10" style="text-align: center;"><h5 style="margin-bottom:1rem;color:#909090">Preencha os campos com as informações do proponente</h5></div>
+<div class="col-md-10" style="text-align: center; margin-top:2rem">
+    <h4 style="margin-top: 1rem;">2º Passo</h4>
+</div>
+<div class="col-md-10" style="text-align: center;">
+    <h5 style="margin-bottom:1rem;color:#909090">Preencha os campos com as informações do proponente</h5>
+</div>
 <div class="col-md-10">
-  <div class="card" style="border-radius: 12px">
-    <div class="card-body">
-      <div class="container">
-        <div class="form-row mt-3">
-          <div class="col-md-12"><h5 style="color: #1492E6; margin-bottom:-0.4rem">Informações do proponente</h5></div>
-          <div class="col-md-12" style="margin-bottom: -0.8rem;"><hr style="border-top: 1px solid#1492E6"></div>
+    <div class="card" style="border-radius: 12px">
+        <div class="card-body">
+            <div class="container">
+                <div class="form-row mt-3">
+                    <div class="col-md-12">
+                        <h5 style="color: #1492E6; margin-bottom:-0.4rem">Informações do proponente</h5>
+                    </div>
+                    <div class="col-md-12" style="margin-bottom: -0.8rem;">
+                        <hr style="border-top: 1px solid#1492E6">
+                    </div>
 
-          <div class="form-group col-md-12" style="margin-top: 15px">
-            <label for="nomeCompletoProponente1">Proponente</label>
-            <input class="form-control" type="text" id="nomeCompletoProponente1" name="nomeCoordenador" disabled="disabled" value="{{ $projeto->proponente->user->name }}">
-          
-          </div>
+                    <div class="form-group col-md-12" style="margin-top: 15px">
+                        <label for="nomeCompletoProponente1">Proponente</label>
+                        <input class="form-control" type="text" id="nomeCompletoProponente1" name="nomeCoordenador"
+                            disabled="disabled" value="{{ $projeto->proponente->user->name }}">
 
-          <div class="form-group col-md-6">
-            <label for="linkLattesEstudante">Link do currículo Lattes<span style="color: red; font-weight:bold">*</span></label>
-            <input class="form-control @error('linkLattesEstudante') is-invalid @enderror" type="text" name="linkLattesEstudante" value="{{$projeto->linkLattesEstudante}}" disabled >
-            <small>Ex.: http://lattes.cnpq.br/8363536830656923</small>
-            @error('linkLattesEstudante')
-            <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
-              <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-          </div>
-          
-          <div class="form-group col-md-6">
-            <label for="linkGrupo">Link do grupo de pesquisa</label>
-            <input class="form-control @error('linkGrupo') is-invalid @enderror" type="url" name="linkGrupo"
-                    value="{{ $projeto->linkGrupoPesquisa }}" disabled>
+                    </div>
 
-            <small>Ex.: http://dgp.cnpq.br/dgp/espelhogrupo/228363</small>
-            @error('linkGrupo')
-            <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
-              <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-          </div>
-          <div class="form-group col-md-6">
-            <label for="pontuacaoPlanilha">Valor da planilha de pontuação <span style="color: red; font-weight:bold">*</span></label>
-            <input class="form-control @error('pontuacaoPlanilha') is-invalid @enderror" type="number" min="0" step=".01" name="pontuacaoPlanilha"
-            value="{{ $projeto->pontuacaoPlanilha }}"  disabled>
+                    <div class="form-group col-md-6">
+                        <label for="linkLattesEstudante">Link do currículo Lattes<span
+                                style="color: red; font-weight:bold">*</span></label>
+                        <input class="form-control @error('linkLattesEstudante') is-invalid @enderror" type="text"
+                            name="linkLattesEstudante" value="{{ $projeto->linkLattesEstudante }}" disabled>
+                        <small>Ex.: http://lattes.cnpq.br/8363536830656923</small>
+                        @error('linkLattesEstudante')
+                            <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
 
-            @error('pontuacaoPlanilha')
-            <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
-              <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-          </div>
-          
+                    <div class="form-group col-md-6">
+                        <label for="linkGrupo">Link do grupo de pesquisa</label>
+                        <input class="form-control @error('linkGrupo') is-invalid @enderror" type="url"
+                            name="linkGrupo" value="{{ $projeto->linkGrupoPesquisa }}" disabled>
+
+                        <small>Ex.: http://dgp.cnpq.br/dgp/espelhogrupo/228363</small>
+                        @error('linkGrupo')
+                            <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="pontuacaoPlanilha">Valor da planilha de pontuação <span
+                                style="color: red; font-weight:bold">*</span></label>
+                        <input class="form-control @error('pontuacaoPlanilha') is-invalid @enderror" type="number"
+                            min="0" step=".01" name="pontuacaoPlanilha"
+                            value="{{ $projeto->pontuacaoPlanilha }}" disabled>
+
+                        @error('pontuacaoPlanilha')
+                            <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+
+                    @if ($projeto->conflitosInteresse != null &&
+                        (Auth::user()->tipo == 'administrador' ||
+                            Auth::user()->tipo == 'administradorResponsavel' ||
+                            Auth::user()->tipo == 'coordenador'))
+                        <div class="form-group col-md-6">
+                            <label for="conflitosInteresse">Conflitos de interesse: </label>
+                            <textarea class="form-control @error('conflitosInteresse') is-invalid @enderror" name="conflitosInteresse"
+                                rows="4" disabled>{{ $projeto->conflitosInteresse }}</textarea>
+                        </div>
+                    @endif
+
+                </div>
+            </div>
         </div>
     </div>
-    </div>
-  </div>
 </div>
 <!--X Proponente X-->
--- resources/views/projeto/formularioVisualizar/proponente2.blade.php
+++ resources/views/projeto/formularioVisualizar/proponente2.blade.php
@@ -1,84 +1,100 @@
 <!-- Proponente -->
 <div class="col-md-12" style="margin-top: 20px">
-  <div class="card" style="border-radius: 5px">
-    <div class="card-body" style="padding-top: 0.2rem;">
-      <div class="container">
-        <div class="form-row mt-3">
-          <div class="col-md-12"><h5 style="color: #234B8B; font-weight: bold">Informações do Proponente</h5></div>
-        </div>
-        <hr style="border-top: 1px solid#1492E6">
-        <div class="form-row mt-3">
-          <div class="col-md-12">
-            <p style="color: #4D4D4D; padding: 0px"><b>Nome:</b> {{ App\Proponente::find($projeto->proponente_id)->user->name }}</p>
-          </div>
-          <div class="col-md-12">
-            <b style="color: #4D4D4D;">Lattes:</b>
-            @if(App\Proponente::where('id', $projeto->proponente_id)->first()->linkLattes != null)
-              <a style="color: #4D4D4D;" href="{{ App\Proponente::where('id', $projeto->proponente_id)->first()->linkLattes }}"
-                 target="_blank"
-              >{{ App\Proponente::where('id', $projeto->proponente_id)->first()->linkLattes }}</a>
-            @endif
-          </div>
-          
-          @if( (Auth::user()->avaliadors != null) && (Auth::user()->avaliadors->tipo != 'Externo' || Auth::user()->avaliadors->tipo == null)
-          && ($edital->natureza_id != 3 || $projeto->status != "rascunho"))
-          <!-- só pagar oq tem dps do || para funcionar para submetido e rascunho! EXTENSÃO(3)!!! -->
-          <div class="col-md-12">
-            <br>
-            <b style="color: #4D4D4D;">Grupo de Pesquisa: </b>
-            <a style="color: #4D4D4D;" href="{{ $projeto->linkGrupoPesquisa }}"
-               target="_blank"
-            >{{ $projeto->linkGrupoPesquisa }}</a>
-          </div>
-          
-          @if(!isset($mostrar_val_planilha))
-            <div class="col-md-12">
-              <br>
-              <b style="color: #4D4D4D;">Valor da Planilha de Pontuação: </b>
-              <a style="color: #4D4D4D;">{{$projeto->pontuacaoPlanilha}}</a>
-            </div>
-          @endif
-          @endif
-
-          @if($edital->natureza_id == 3)
-            <div class="col-md-12">
-              <br>
-              <b style="color: #4D4D4D;">Área Temática:</b>
-              <a style="color: #4D4D4D;">{{App\AreaTematica::where('id', $projeto->area_tematica_id)->first()->nome}}</a>
-            </div>
-            <div class="row col-md-12">
-                    <div class="form-group col-md-12">
-                    <br>
-                    <b style="color: #4D4D4D;">Objetivo de Densenvolvimento Sustentável:</b>
-                    <br>
-                    <div class="row col-md-12">
-                      @foreach($projeto->ods as $ods)
-                      <div class="col-md-6">
-                        <a style="color: #4D4D4D;">
-                          {{ $ods->nome }}
-                        </a>
-                      </div>
-                      @endforeach
+    <div class="card" style="border-radius: 5px">
+        <div class="card-body" style="padding-top: 0.2rem;">
+            <div class="container">
+                <div class="form-row mt-3">
+                    <div class="col-md-12">
+                        <h5 style="color: #234B8B; font-weight: bold">Informações do Proponente</h5>
                     </div>
-                  </div>
-                </div> 
-          @endif
+                </div>
+                <hr style="border-top: 1px solid#1492E6">
+                <div class="form-row mt-3">
+                    <div class="col-md-12">
+                        <p style="color: #4D4D4D; padding: 0px"><b>Nome:</b>
+                            {{ App\Proponente::find($projeto->proponente_id)->user->name }}</p>
+                    </div>
+                    <div class="col-md-12">
+                        <b style="color: #4D4D4D;">Lattes:</b>
+                        @if (App\Proponente::where('id', $projeto->proponente_id)->first()->linkLattes != null)
+                            <a style="color: #4D4D4D;"
+                                href="{{ App\Proponente::where('id', $projeto->proponente_id)->first()->linkLattes }}"
+                                target="_blank">{{ App\Proponente::where('id', $projeto->proponente_id)->first()->linkLattes }}</a>
+                        @endif
+                    </div>
 
-          @if($projeto->modalidade!=null)
-            <div class="col-md-12">
-              <br>
-              <b style="color: #4D4D4D;">Modalidade: </b>
-              @if($projeto->modalidade=="RecemDoutor")
-                <a style="color: #4D4D4D;">Recém Doutor</a>
-              @else
-                <a style="color: #4D4D4D;">Ampla Concorrência</a>
-              @endif
+                    @if (Auth::user()->avaliadors != null &&
+                        (Auth::user()->avaliadors->tipo != 'Externo' || Auth::user()->avaliadors->tipo == null) &&
+                        ($edital->natureza_id != 3 || $projeto->status != 'rascunho'))
+                        <!-- só pagar oq tem dps do || para funcionar para submetido e rascunho! EXTENSÃO(3)!!! -->
+                        <div class="col-md-12">
+                            <br>
+                            <b style="color: #4D4D4D;">Grupo de Pesquisa: </b>
+                            <a style="color: #4D4D4D;" href="{{ $projeto->linkGrupoPesquisa }}"
+                                target="_blank">{{ $projeto->linkGrupoPesquisa }}</a>
+                        </div>
+
+                        @if (!isset($mostrar_val_planilha))
+                            <div class="col-md-12">
+                                <br>
+                                <b style="color: #4D4D4D;">Valor da Planilha de Pontuação: </b>
+                                <a style="color: #4D4D4D;">{{ $projeto->pontuacaoPlanilha }}</a>
+                            </div>
+                        @endif
+                    @endif
+
+                    @if ($edital->natureza_id == 3)
+                        <div class="col-md-12">
+                            <br>
+                            <b style="color: #4D4D4D;">Área Temática:</b>
+                            <a
+                                style="color: #4D4D4D;">{{ App\AreaTematica::where('id', $projeto->area_tematica_id)->first()->nome }}</a>
+                        </div>
+                        <div class="row col-md-12">
+                            <div class="form-group col-md-12">
+                                <br>
+                                <b style="color: #4D4D4D;">Objetivo de Densenvolvimento Sustentável:</b>
+                                <br>
+                                <div class="row col-md-12">
+                                    @foreach ($projeto->ods as $ods)
+                                        <div class="col-md-6">
+                                            <a style="color: #4D4D4D;">
+                                                {{ $ods->nome }}
+                                            </a>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+
+                    @if ($projeto->modalidade != null)
+                        <div class="col-md-12">
+                            <br>
+                            <b style="color: #4D4D4D;">Modalidade: </b>
+                            @if ($projeto->modalidade == 'RecemDoutor')
+                                <a style="color: #4D4D4D;">Recém Doutor</a>
+                            @else
+                                <a style="color: #4D4D4D;">Ampla Concorrência</a>
+                            @endif
+                        </div>
+                    @endif
+
+                    @if ($projeto->conflitosInteresse != null &&
+                        (Auth::user()->tipo == 'administrador' ||
+                            Auth::user()->tipo == 'administradorResponsavel' ||
+                            Auth::user()->tipo == 'coordenador' ||
+                            Auth::user()->id == $projeto->proponente->user->id))
+                        <div class="col-md-12">
+                            <br>
+                            <b style="color: #4D4D4D;">Conflitos de Interesse: </b>
+                            <a style="color: #4D4D4D;">{{ $projeto->conflitosInteresse }}</a>
+                        </div>
+                    @endif
+
+                </div>
             </div>
-          @endif
-
         </div>
-      </div>
     </div>
-  </div>
 </div>
 <!--X Proponente X-->
