9cfd39217af6790079a808c5b844be511da57609
unknown <antoniodurvalc@gmail.com>
2022-12-12 14:57:42 -0300

    Listando apenas os avaliadores de extensão baseado na natureza do edital

--- resources/views/administrador/selecionarAvaliadores.blade.php
+++ resources/views/administrador/selecionarAvaliadores.blade.php
@@ -43,7 +43,11 @@
       <tr>   
         <th scope="col">Nome do Usuário</th>
         <th scope="col">Email</th>
-        <th scope="col">Área</th>
+        @if($evento->natureza_id == 3)
+          <th scope="col">Área Temática</th>
+        @else
+          <th scope="col">Área</th>
+        @endif
         <th scope="col">Tipo</th>
         <th scope="col" style="text-align:center">Ação</th>
       </tr>
@@ -55,7 +59,11 @@
           <td>{{ $avaliador->user->email }}</td>
           <td>
             @if(is_null($avaliador->area))
-              Indefinida
+              @if($avaliador->areaTematicas()->get()->first() != null)
+                {{ $avaliador->areaTematicas()->get()->first()->nome }}
+              @else
+                Indefinida
+              @endif
             @else
               {{ $avaliador->area->nome }}
             @endif
@@ -71,7 +79,9 @@
           <td style="text-align:center">
             <form action="{{ route('admin.adicionar') }}" method="POST">
               @csrf
-              <input type="hidden" name="avaliador_id" value="{{ $avaliador->id }}" >
+              <!-- Possibilidade de exclusão -->
+              {{-- <input type="hidden" name="avaliador_id" value="{{ $avaliador->avaliador_id }}"> --}}
+              <input type="hidden" name="avaliador_id" value="{{ $avaliador->id }}" > 
               <input type="hidden" name="evento_id" value="{{ $evento->id }}" >
               <button type="submit" class="btn btn-primary" >Adicionar</button>
             </form>          
@@ -251,7 +261,8 @@
             <label for="exampleInputEmail1">Email <span style="color: red;">*</span></label>
             <input type="email" class="form-control" name="emailAvaliador" id="exampleInputEmail1" required>            
           </div>
-
+  <!-- aki -->
+        @if($evento->natureza_id != 3)
           <div class="form-group">
           <label for="grandeArea" class="col-form-label">{{ __('Grande Área') }} <span style="color: red; font-weight:bold">*</span></label>
               <select class="form-control" id="grandeArea" name="grande_area_id" onchange="areas()" required>
@@ -272,6 +283,22 @@
               <option value="avaliador" >Avaliador</option>
             </select>
           </div>
+        @else
+       
+          <div class="form-group">
+              <label for="grandeArea" class="col-form-label">{{ __('Áreas Temáticas') }} <span
+                              style="color: red; font-weight:bold">*</span></label>
+                  <select class="form-control" id="grandeAreaConvite" name="area_tematica_id"
+                  required>
+                          <option value="" disabled selected hidden>-- Áreas Temáticas --</option>
+                      @foreach($areasTematicas as $areaTematica)
+                          <option value="{{$areaTematica->id}}">{{$areaTematica->nome}}</option>
+                      @endforeach
+                  </select>
+          </div>
+                        
+
+        @endif
   
           @if($evento->natureza_id != 3)
             <div class="form-group">
