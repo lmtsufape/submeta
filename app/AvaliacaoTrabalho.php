467aa5ea317e6f8549cadc87d446cb28697e719e
Lucas Henrique <lucasmonte2011@gmail.com>
2022-12-14 15:38:00 -0300

    Adiciona soft delete para modelo AvaliacaoTrabalho

--- app/AvaliacaoTrabalho.php
+++ app/AvaliacaoTrabalho.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AvaliacaoTrabalho extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'nota',
         'avaliador_id',
