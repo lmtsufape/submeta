36735a0da2608ef289f5825953732a8e86e72427
Lucas Henrique <lucasmonte2011@gmail.com>
2022-12-16 10:35:08 -0300

    Criando controllers de CampoAvaliacao e AvaliacaoTrabalho

---
+++ app/Http/Controllers/AvaliacaoTrabalhoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\AvaliacaoTrabalho;
+use App\CampoAvaliacao;
+
+class AvaliacaoTrabalhoController extends Controller
+{
+    //
+}
