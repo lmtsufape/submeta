cdd1adacbccac10479867fb700590405581f16b4
Yuri Resende <yresendeo@gmail.com>
2023-05-12 17:22:39 -0300

    Possibilitando a criação do edital pelo administrador

--- routes/web.php
+++ routes/web.php
@@ -324,7 +324,7 @@ Route::prefix('cursos')->name('cursos.')->group(function (){
 //############ Evento ##############################################
 Route::prefix('evento')->name('evento.')->group(function () {
     Route::get('/criar', 'EventoController@create')->name('criar')->middleware('checkRoles:coordenador,administrador');
-    Route::post('/criar', 'EventoController@store')->name('criar')->middleware('checkRoles:coordenador,checkAdministrador');
+    Route::post('/criar', 'EventoController@store')->name('criar')->middleware('checkRoles:coordenador,administrador');
     Route::get('/visualizar/{id}', 'EventoController@show')->name('visualizar')->middleware('auth');
     Route::get('/listar', 'EventoController@listar')->name('listar')->middleware('auth');
     Route::delete('/excluir/{id}', 'EventoController@destroy')->name('deletar')->middleware('checkRoles:coordenador,administrador');
