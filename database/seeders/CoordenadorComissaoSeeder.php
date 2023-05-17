62386f44e5f3800e99bfc345c351471ef778eef9
luiz-davi <luizd4398@gmail.com>
2023-05-17 19:21:24 -0300

    adicionando usuários + linkando com coordenador_comissaos

--- database/seeders/CoordenadorComissaoSeeder.php
+++ database/seeders/CoordenadorComissaoSeeder.php
@@ -11,13 +11,25 @@ class CoordenadorComissaoSeeder extends Seeder
      */
     public function run()
     {
-        $user_id = DB::table('users')->where('name','Coordenador1')->pluck('id');
+        $user_id = DB::table('users')->where('name', 'Coordenador1')->pluck('id');
 
-      	DB::table('coordenador_comissaos')->insert([
-        	'user_id' => $user_id[0],
-      	]);
+        DB::table('coordenador_comissaos')->insert([
+            'user_id' => $user_id[0],
+        ]);
 
-        $user_id = DB::table('users')->where('name','Coordenador2')->pluck('id');
+        $user_id = DB::table('users')->where('name', 'Coordenador2')->pluck('id');
+
+        DB::table('coordenador_comissaos')->insert([
+            'user_id' => $user_id[0],
+        ]);
+
+        $user_id = DB::table('users')->where('email', 'planejamento.prec@ufape.edu.br')->pluck('id');
+
+        DB::table('coordenador_comissaos')->insert([
+            'user_id' => $user_id[0],
+        ]);
+
+        $user_id = DB::table('users')->where('email', 'coorplan.prec@ufape.edu.br')->pluck('id');
 
         DB::table('coordenador_comissaos')->insert([
             'user_id' => $user_id[0],
