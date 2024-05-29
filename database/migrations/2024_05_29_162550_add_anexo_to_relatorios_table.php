592e3237737a1c43c58595d24f4c41dbfd1f4c07
Douglas Batista <86257838+douglasfsbatista@users.noreply.github.com>
2024-05-29 17:31:40 -0300

    Update 2024_05_29_162550_add_anexo_to_relatorios_table.php

--- database/migrations/2024_05_29_162550_add_anexo_to_relatorios_table.php
+++ database/migrations/2024_05_29_162550_add_anexo_to_relatorios_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnAnexoToRelatoriosTable extends Migration
+class AddAnexoToRelatoriosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,7 @@ class AddColumnAnexoToRelatoriosTable extends Migration
     {
         Schema::table('relatorios', function (Blueprint $table)
         {
-            $table->string('anexo');
+            $table->string('anexo')->nullable();
         });
     }
 
