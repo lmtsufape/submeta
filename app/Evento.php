a20344e38a103c79726b97b421bc3eb69a44f693
Douglas Filipe <batista.douglasfs@gmail.com>
2025-07-03 16:09:33 -0300

    feat: atualização do model 'Evento' para o edital 'Programas de Extensão'

--- app/Evento.php
+++ app/Evento.php
@@ -14,60 +14,78 @@ class Evento extends Model
    * @var array
    */
   protected $fillable = [
-      'nome', 'descricao', 'tipo',
-      'inicioSubmissao', 'fimSubmissao', 'inicioRevisao', 'fimRevisao',
-      'resultado_final','resultado_preliminar', 'coordenadorId',
-      'numMaxTrabalhos', 'numMaxCoautores', 'hasResumo', 'criador_id', 'numParticipantes',
-      'dt_inicioRelatorioParcial', 'dt_fimRelatorioParcial', 'dt_inicioRelatorioFinal', 'dt_fimRelatorioFinal',
-      'formAvaliacaoExterno', 'formAvaliacaoInterno',
-      'cotaDoutor', 'inicioProjeto', 'fimProjeto',
-      'formAvaliacaoRelatorio', 'docTutorial'
+    'nome',
+    'descricao',
+    'tipo',
+    'inicioSubmissao',
+    'fimSubmissao',
+    'inicioRevisao',
+    'fimRevisao',
+    'resultado_final',
+    'resultado_preliminar',
+    'coordenadorId',
+    'numMaxTrabalhos',
+    'numMaxCoautores',
+    'hasResumo',
+    'criador_id',
+    'numParticipantes',
+    'dt_inicioRelatorioParcial',
+    'dt_fimRelatorioParcial',
+    'dt_inicioRelatorioFinal',
+    'dt_fimRelatorioFinal',
+    'formAvaliacaoExterno',
+    'formAvaliacaoInterno',
+    'cotaDoutor',
+    'inicioProjeto',
+    'fimProjeto',
+    'formAvaliacaoRelatorio',
+    'docTutorial'
   ];
 
   #validação para as datas dos editais padrão
   public static $dates_rules = [
     'inicioSubmissao'           => ['required', 'date'],
     'fimSubmissao'              => ['required', 'date'],
-    'inicioRevisao'             => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP', 'required', 'date'],
-    'fimRevisao'                => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP', 'required', 'date'],
-    'inicio_recurso'            => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP', 'required', 'date'],
-    'fim_recurso'               => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP', 'required', 'date'],
-    'resultado_final'           => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP', 'required', 'date'],
-    'resultado_preliminar'      => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP', 'required', 'date'],
-    'dt_inicioRelatorioParcial' => ['exclude_if:tipo,PIBEX,PIACEX,PIBAC,CONTINUO,CONTINUO-AC,PICP', 'required', 'date'],
-    'dt_fimRelatorioParcial'    => ['exclude_if:tipo,PIBEX,PIACEX,PIBAC,CONTINUO,CONTINUO-AC,PICP', 'required', 'date'],
+    'inicioRevisao'             => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date'],
+    'fimRevisao'                => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date'],
+    'inicio_recurso'            => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date'],
+    'fim_recurso'               => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date'],
+    'resultado_final'           => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date'],
+    'resultado_preliminar'      => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date'],
+    'dt_inicioRelatorioParcial' => ['exclude_if:tipo,PIBEX,PIACEX,PIBAC,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date'],
+    'dt_fimRelatorioParcial'    => ['exclude_if:tipo,PIBEX,PIACEX,PIBAC,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date'],
     'dt_inicioRelatorioFinal'   => ['required', 'date'],
     'dt_fimRelatorioFinal'      => ['required', 'date'],
-    'inicioProjeto'             => ['exclude_if:tipo,CONTINUO,CONTINUO-AC', 'required', 'date'],
-    'fimProjeto'                => ['exclude_if:tipo,CONTINUO,CONTINUO-AC', 'required', 'date'],
+    'inicioProjeto'             => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PROGRAMAS-EXTENSAO', 'required', 'date'],
+    'fimProjeto'                => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PROGRAMAS-EXTENSAO', 'required', 'date'],
     //'modeloDocumento'     => [],
   ];
 
   #validação completa dos dados de editais padrão
   public static $rules = [
     'nome'                      => ['required', 'string'],
-    'descricao'                 => ['required', 'string','max:1500'],
+    'descricao'                 => ['required', 'string', 'max:1500'],
     'tipo'                      => ['required', 'string'],
     'natureza'                  => ['required'],
     'coordenador_id'            => ['required'],
     'numParticipantes'          => ['required'],
-    'nome_docExtra'             => ['required_with:check_docExtra','max:255'],
+    'nome_docExtra'             => ['required_with:check_docExtra', 'max:255'],
     'tipoAvaliacao'             => ['required'],
     'inicioSubmissao'           => ['required', 'date', 'after:yesterday'],
     'fimSubmissao'              => ['required', 'date', 'after_or_equal:inicioSubmissao'],
-    'inicioRevisao'             => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP', 'required', 'date', 'after:yesterday'],
-    'fimRevisao'                => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP', 'required', 'date', 'after:inicioRevisao', 'after:fimSubmissao'],
-    'resultado_preliminar'      => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP', 'required', 'date', 'after_or_equal:fimRevisao'],
-    'inicio_recurso'            => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP', 'required', 'date', 'after_or_equal:resultado_preliminar'],
-    'fim_recurso'               => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP', 'required', 'date', 'after:inicio_recurso'],
-    'resultado_final'           => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP', 'required', 'date', 'after:fim_recurso'],
-    'dt_inicioRelatorioParcial' => ['exclude_if:tipo,PIBEX,PIACEX,PIBAC,CONTINUO,CONTINUO-AC,PICP', 'required', 'date', 'after:resultado_final'],
-    'dt_fimRelatorioParcial'    => ['exclude_if:tipo,PIBEX,PIACEX,PIBAC,CONTINUO,CONTINUO-AC,PICP', 'required', 'date', 'after_or_equal:dt_inicioRelatorioParcial'],
+    'inicioRevisao'             => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date', 'after:yesterday'],
+    'fimRevisao'                => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date', 'after:inicioRevisao', 'after:fimSubmissao'],
+    'resultado_preliminar'      => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date', 'after_or_equal:fimRevisao'],
+    'inicio_recurso'            => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date', 'after_or_equal:resultado_preliminar'],
+    'fim_recurso'               => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date', 'after:inicio_recurso'],
+    'resultado_final'           => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date', 'after:fim_recurso'],
+    'dt_inicioRelatorioParcial' => ['exclude_if:tipo,PIBEX,PIACEX,PIBAC,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date', 'after:resultado_final'],
+    'dt_fimRelatorioParcial'    => ['exclude_if:tipo,PIBEX,PIACEX,PIBAC,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date', 'after_or_equal:dt_inicioRelatorioParcial'],
     'dt_inicioRelatorioFinal'   => ['required', 'date', 'after:dt_fimRelatorioParcial'],
     'dt_fimRelatorioFinal'      => ['required', 'date', 'after_or_equal:dt_inicioRelatorioFinal'],
-    'pdfEdital'                 => [('pdfEditalPreenchido'!=='sim'?'required':''), 'file', 'mimes:pdf', 'max:2048'],
-    'inicioProjeto'             => ['exclude_if:tipo,CONTINUO,CONTINUO-AC', 'required', 'date', 'after:yesterday'],
-    'fimProjeto'                => ['exclude_if:tipo,CONTINUO,CONTINUO-AC', 'required', 'date', 'after_or_equal:fimSubmissao'],
+    'pdfEdital'                 => [('pdfEditalPreenchido' !== 'sim' ? 'required' : ''), 'file', 'mimes:pdf', 'max:2048'],
+    'inicioProjeto'             => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PROGRAMAS-EXTENSAO', 'required', 'date', 'after:yesterday'],
+    'fimProjeto'                => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PROGRAMAS-EXTENSAO', 'required', 'date', 'after_or_equal:fimSubmissao'],
   ];
 
   public static $edit_rules = [
@@ -79,66 +97,73 @@ class Evento extends Model
     'tipoAvaliacao'       => ['required'],
     'inicioSubmissao'     => ['required', 'date', 'after_or_equal:inicioSubmissao'],
     'fimSubmissao'        => ['required', 'date', 'after_or_equal:inicioSubmissao'],
-    'inicioRevisao'       => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP', 'required', 'date', 'after:fimSubmissao'],
-    'fimRevisao'          => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP', 'required', 'date', 'after:inicioRevisao'],
-    'resultado_preliminar'=> ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP', 'required', 'date', 'after_or_equal:fimRevisao'],
-    'inicio_recurso'      => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP', 'required', 'date', 'after_or_equal:resultado_preliminar'],
-    'fim_recurso'         => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP', 'required', 'date', 'after:inicio_recurso'],
-    'resultado_final'     => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP', 'required', 'date', 'after:fim_recurso'],
-    'dt_inicioRelatorioParcial'  => ['exclude_if:tipo,PIBEX,PIACEX,PIBAC,CONTINUO,CONTINUO-AC,PICP', 'required', 'date', 'after:resultado_final'],
-    'dt_fimRelatorioParcial'     => ['exclude_if:tipo,PIBEX,PIACEX,PIBAC,CONTINUO,CONTINUO-AC,PICP', 'required', 'date', 'after_or_equal:dt_inicioRelatorioParcial'],
+    'inicioRevisao'       => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date', 'after:fimSubmissao'],
+    'fimRevisao'          => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date', 'after:inicioRevisao'],
+    'resultado_preliminar' => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date', 'after_or_equal:fimRevisao'],
+    'inicio_recurso'      => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date', 'after_or_equal:resultado_preliminar'],
+    'fim_recurso'         => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date', 'after:inicio_recurso'],
+    'resultado_final'     => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date', 'after:fim_recurso'],
+    'dt_inicioRelatorioParcial'  => ['exclude_if:tipo,PIBEX,PIACEX,PIBAC,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date', 'after:resultado_final'],
+    'dt_fimRelatorioParcial'     => ['exclude_if:tipo,PIBEX,PIACEX,PIBAC,CONTINUO,CONTINUO-AC,PICP,PROGRAMAS-EXTENSAO', 'required', 'date', 'after_or_equal:dt_inicioRelatorioParcial'],
     'dt_inicioRelatorioFinal'  => ['required', 'date', 'after:dt_fimRelatorioParcial'],
     'dt_fimRelatorioFinal'     => ['required', 'date', 'after_or_equal:dt_inicioRelatorioFinal'],
     'modeloDocumento.*'     => ['file', 'mimes:zip,doc,docx,odt,pdf', 'max:2048'],
     'pdfFormAvalExterno'           => ['file', 'mimes:pdf,doc,docx,xlsx,xls,csv,zip', 'max:2048'],
-    'inicioProjeto'       => ['exclude_if:tipo,CONTINUO,CONTINUO-AC', 'required', 'date', 'after:resultado_final'],
-    'fimProjeto'          => ['exclude_if:tipo,CONTINUO,CONTINUO-AC', 'required', 'date', 'after:inicioProjeto'],
+    'inicioProjeto'       => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PROGRAMAS-EXTENSAO', 'required', 'date', 'after:resultado_final'],
+    'fimProjeto'          => ['exclude_if:tipo,CONTINUO,CONTINUO-AC,PROGRAMAS-EXTENSAO', 'required', 'date', 'after:inicioProjeto'],
     'docTutorial'     => ['file', 'mimes:zip,doc,docx,pdf', 'max:2048'],
     'nome_docExtra'       => ['required_with:check_docExtra', 'max:255'],
   ];
-  
+
 
   public static $continuos_edit_rules = [
     'nome'                => ['required', 'string'],
-    'descricao'           => ['required', 'string','max:1500'],
+    'descricao'           => ['required', 'string', 'max:1500'],
     'tipo'                => ['required', 'string'],
     'natureza'            => ['required'],
     'coordenador_id'      => ['required'],
     'numParticipantes'    => ['required'],
-    'nome_docExtra'       => ['required_with:check_docExtra','max:255'],
+    'nome_docExtra'       => ['required_with:check_docExtra', 'max:255'],
     'tipoAvaliacao'       => ['required'],
     'inicioSubmissao'     => ['required', 'date', 'after_or_equal:inicioSubmissao'],
     'fimSubmissao'        => ['required', 'date', 'after_or_equal:inicioSubmissao'],
     'pdfEdital'           => ['sometimes', 'required', 'file', 'mimes:pdf', 'max:2048'],
   ];
 
-  public function endereco(){
-      return $this->belongsTo('App\Endereco', 'enderecoId');
+  public function endereco()
+  {
+    return $this->belongsTo('App\Endereco', 'enderecoId');
   }
 
-  public function atividade(){
-      return $this->hasOne('App\Atividade', 'eventoId');
+  public function atividade()
+  {
+    return $this->hasOne('App\Atividade', 'eventoId');
   }
 
-  public function area(){
-      return $this->hasOne('App\Area', 'eventoId');
+  public function area()
+  {
+    return $this->hasOne('App\Area', 'eventoId');
   }
 
-  public function coordenador(){
-      return $this->belongsTo('App\User', 'coordenadorId');
+  public function coordenador()
+  {
+    return $this->belongsTo('App\User', 'coordenadorId');
   }
 
-  public function coordenadorComissao(){
-      return $this->belongsTo('App\CoordenadorComissao', 'coordenadorId');
+  public function coordenadorComissao()
+  {
+    return $this->belongsTo('App\CoordenadorComissao', 'coordenadorId');
   }
-  public function trabalhos(){
-      return $this->hasMany('App\Trabalho');
+  public function trabalhos()
+  {
+    return $this->hasMany('App\Trabalho');
   }
-  public function avaliadors(){
-      return $this->belongsToMany('App\Avaliador')->withPivot('convite', 'created_at');
+  public function avaliadors()
+  {
+    return $this->belongsToMany('App\Avaliador')->withPivot('convite', 'created_at');
   }
-  public function campos_avaliacao(){
+  public function campos_avaliacao()
+  {
     return $this->hasMany('App\CampoAvaliacao');
-}
-
+  }
 }
