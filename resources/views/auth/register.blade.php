89d425f13bdc6512b1d6c01a64bedef7821715cc
cauaofsouza <cauasantos064@gmail.com>
2026-05-02 09:27:36 -0300

    bugfix(register.blade): corrige mascara de telefone Closes #1012

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -639,7 +639,9 @@
         $(document).ready(function ($) {
             $('#cpf').mask('000.000.000-00');
             var SPMaskBehavior = function (val) {
-                    return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+                    return val.replace(/\D/g, '').length < 11
+                        ? '(00) 0000-00009'   //isso faz com que possa ser digitado o digito do proximo if
+                        : '(00) 00000-0000';
                 },
                 spOptions = {
                     onKeyPress: function (val, e, field, options) {
@@ -650,6 +652,7 @@
             $('#SIAPE').mask('00000000');
             $('#AnoTitulacao').mask('0000');
             $('#cep').mask('00000-000');
+            $('#data_de_nascimento').mask('00/00/0000');
         });
 
         function mudarPerfil() {
