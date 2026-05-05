a052995976a18ef90c6e675f5e7cb3825132ce6d
cauaofsouza <cauasantos064@gmail.com>
2026-05-04 22:21:54 -0300

    refact(UserController): refatora metodo de editar perfil
    -adiciona formrequest para validacao dos dados da requisicao de edicao
    -adiciona service para manusear logica
    -remove ambos do controller
    Closes #1017

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateProfileRequest;
+use App\Services\UserService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
@@ -70,164 +72,9 @@ class UserController extends Controller
             ]);
     }
 
-    function editarPerfil(Request $request)
+    function editarPerfil(UpdateProfileRequest $request, UserService $service)
     {
-        $id = Auth()->user()->id;
-        $user = User::find($id);
-        if ($request->tipo != "proponente") {
-            $validated = $request->validate([
-                'name' => ['required', 'string', 'max:255'],
-                'instituicao' => ['required_if:instituicaoSelect,Outra', 'max:255'],
-                'instituicaoSelect' => ['required_without:instituicao'],
-                'celular' =>  ['required', 'string'],
-                'cpf' => ['required', 'cpf'],
-            ]);
-        } else {
-            $validated = $request->validate([
-                'name' => ['required', 'string', 'max:255'],
-                //'email' => ['required', 'string', 'email', 'max:255'],
-                'cpf' => ['required', 'cpf'],
-                'celular' => ['required', 'string'],
-                'instituicao' => ['required_if:instituicaoSelect,Outra', 'max:255'],
-                'instituicaoSelect' => ['required_without:instituicao'],
-                'cargo' => ['required'],
-                'vinculo' => ['required'],
-                'outro' => ['required_if:vinculo,Outro'],
-                'titulacaoMaxima' => ['required_with:anoTitulacao,areaFormacao,bolsistaProdutividade'],
-                'titulacaoMaxima' => Rule::requiredIf((isset($request['cargo']) && $request['cargo'] !== 'Estudante') || (isset($request['cargo']) && $request['cargo'] === 'Estudante' && isset($request['vinculo']) && $request['vinculo'] === 'Pós-doutorando')),
-                'anoTitulacao' => ['required_with:titulacaoMaxima,areaFormacao,bolsistaProdutividade,linkLattes'],
-                'anoTitulacao' => Rule::requiredIf((isset($request['cargo']) && $request['cargo'] !== 'Estudante') || (isset($request['cargo']) && $request['cargo'] === 'Estudante' && isset($request['vinculo']) && $request['vinculo'] === 'Pós-doutorando')),
-                'areaFormacao' => ['required_with:titulacaoMaxima,anoTitulacao,bolsistaProdutividade,linkLattes'],
-                'areaFormacao' => Rule::requiredIf((isset($request['cargo']) && $request['cargo'] !== 'Estudante') || (isset($request['cargo']) && $request['cargo'] === 'Estudante' && isset($request['vinculo']) && $request['vinculo'] === 'Pós-doutorando')),
-                'bolsistaProdutividade' => ['required_with:titulacaoMaxima,anoTitulacao,areaFormacao,linkLattes'],
-                'bolsistaProdutividade' => Rule::requiredIf((isset($request['cargo']) && $request['cargo'] !== 'Estudante') || (isset($request['cargo']) && $request['cargo'] === 'Estudante' && isset($request['vinculo']) && $request['vinculo'] === 'Pós-doutorando')),
-                'nivel' => ['required_if:bolsistaProdutividade,sim'],
-                // 'nivel' => [(isset($request['cargo']) && $request['cargo'] !== 'Estudante') || (isset($request['cargo']) && $request['cargo'] === 'Estudante' && isset($request['vinculo']) && $request['vinculo'] === 'Pós-doutorando') ? 'required':''],
-                'linkLattes' => ['required_with:titulacaoMaxima,anoTitulacao,areaFormacao,bolsistaProdutividade'],
-                'linkLattes' => [(isset($request['cargo']) && $request['cargo'] !== 'Estudante') || (isset($request['cargo']) && $request['cargo'] === 'Estudante' && isset($request['vinculo']) && $request['vinculo'] === 'Pós-doutorando') ? 'required' : ''],
-                'linkLattes' => [(isset($request['cargo']) && $request['cargo'] !== 'Estudante') || (isset($request['cargo']) && $request['cargo'] === 'Estudante' && isset($request['vinculo']) && $request['vinculo'] === 'Pós-doutorando') ? 'link_lattes' : ''],
-
-            ]);
-        }
-
-        if ($user->tipo == 'participante') {
-            $validated = $request->validate([
-                'name' => ['required', 'string', 'max:255'],
-                // 'email' => ['required_if:alterarSenhaCheckBox,on', 'string', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
-                // 'password' => ['required_if:alterarSenhaCheckBox,on', 'string', 'min:8', 'confirmed'],
-                'cpf' => ['required', 'cpf', Rule::unique('users')->ignore($user->id)],
-                //'rg' => ['required', Rule::unique('participantes')->ignore($user->participantes->first()->id)],
-                'celular' => ['required', 'string', 'telefone'],
-                'instituicao' => ['required_if:instituicaoSelect,Outra', 'max:255'],
-                'instituicaoSelect' => ['required_without:instituicao'],
-                'outroCursoEstudante' => ['required_if:cursoEstudante,Outro', 'max:255'],
-                'cursoEstudante' => ['required_without:outroCursoEstudante'],
-                'perfil' => ['required'],
-                'linkLattes' => ['required', 'url'],
-            ]);
-        }
-
-
-        if ($request->alterarSenhaCheckBox != null) {
-            if (!(Hash::check($request->senha_atual, $user->password))) {
-                return redirect()->back()->withErrors(['senha_atual' => 'Senha atual não correspondente']);
-            }
-
-            if (!($request->nova_senha === $request->confirmar_senha)) {
-                return redirect()->back()->withErrors(['nova_senha' => 'Senhas diferentes']);
-            }
-        }
-
-        if($user->avaliadors != null && $request->area != null && $user->tipo == "avaliador"){
-            $avaliador = Avaliador::where('user_id', '=', $id)->first();
-            $avaliador->user_id = $user->id;
-            //$avaliador->area_id = $request->area;
-            $avaliador->naturezas()->sync($request->natureza);
-            $avaliador->update();
-
-        }
-
-        switch ($user->tipo) {
-            case "administradorResponsavel":
-                $adminResp = AdministradorResponsavel::where('user_id', '=', $id)->first();
-                $adminResp->user_id = $user->id;
-                $adminResp->update();
-                break;
-            case "avaliador":
-                $avaliador = Avaliador::where('user_id', '=', $id)->first();
-                $avaliador->user_id = $user->id;
-                //$avaliador->area_id = $request->area;
-                $avaliador->areaTematicas()->sync($request->area);
-                if ($user->usuarioTemp == true) {
-                    $user->usuarioTemp = false;
-                }
-                $avaliador->update();
-                break;
-            case "proponente":
-                $proponente = Proponente::where('user_id', '=', $id)->first();
-                if ($request->SIAPE != null) {
-                    $proponente->SIAPE = $request->SIAPE;
-                }
-                $proponente->cargo = $request->cargo;
-
-                if ($request->vinculo != 'Outro') {
-                    $proponente->vinculo = $request->vinculo;
-                } else {
-                    $proponente->vinculo = $request->outro;
-                }
-
-                $proponente->titulacaoMaxima = $request->titulacaoMaxima;
-                $proponente->anoTitulacao = $request->anoTitulacao;
-                $proponente->areaFormacao = $request->areaFormacao;
-                $proponente->bolsistaProdutividade = $request->bolsistaProdutividade;
-                if ($request->bolsistaProdutividade == 'sim') {
-                    $proponente->nivel = $request->nivel;
-                }
-                $proponente->linkLattes = $request->linkLattes;
-
-                $proponente->user_id = $user->id;
-                $proponente->cursos()->sync($request->curso);
-                $proponente->update();
-                break;
-            case "participante":
-                $participante = $user->participantes()->first();
-                $participante->data_de_nascimento = $request->data_de_nascimento;
-                $participante->linkLattes = $request->linkLattes;
-                $participante->rg = $request->rg;
-                if ($request->outroCursoEstudante != null) {
-                    $participante->curso = $request->outroCursoEstudante;
-                } else if (isset($request->cursoEstudante) && $request->cursoEstudante != "Outro") {
-                    $participante->curso_id = $request->cursoEstudante;
-                }
-                $user->usuarioTemp = false;
-                $endereco = $user->endereco;
-                $endereco->cep = $request->cep;
-                $endereco->uf = $request->uf;
-                $endereco->cidade = $request->cidade;
-                $endereco->rua = $request->rua;
-                $endereco->numero = $request->numero;
-                $endereco->bairro = $request->bairro;
-                $endereco->complemento = $request->complemento;
-                $endereco->update();
-                $participante->update();
-                break;
-        }
-
-        $user->name = $request->name;
-        $user->cpf = $request->cpf;
-        $user->celular = $request->celular;
-        if ($request->instituicao != null) {
-            $user->instituicao = $request->instituicao;
-        } else if (isset($request->instituicaoSelect) && $request->instituicaoSelect != "Outra") {
-            $user->instituicao = $request->instituicaoSelect;
-        }
-
-        if ($request->alterarSenhaCheckBox != null) {
-            $user->password = bcrypt($request->nova_senha);
-        }
-
-
-        $user->update();
+        $service->updateProfile($request->user(), $request->validated());
 
         return redirect(route('user.perfil'))->with(['mensagem' => 'Dados atualizados com sucesso.']);
     }
