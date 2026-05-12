5e2dfa01a1883a73600a5c35d07d038ad97b2611
cauaofsouza <cauasantos064@gmail.com>
2026-05-12 01:16:59 -0300

    refact(substituirParticipante.blade): move modals de erros de substituicao da view para arquivo

---
+++ resources/views/administrador/partials/modals/substituicao-global-errors.blade.php
@@ -0,0 +1,39 @@
+<!-- Modal: CPF não encontrado -->
+<div class="modal fade" id="modal-cpf-nao-encontrado" data-backdrop="static" tabindex="-1" aria-labelledby="modalCpfNaoEncontradoLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header" style="background-color: #dc3545;">
+                <h5 class="modal-title" id="modalCpfNaoEncontradoLabel" style="color: white;">Aviso</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <span id="texto-erro">CPF não consta no sistema!</span>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Ok</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal: Perfil de participante incompleto -->
+<div class="modal fade" id="modal-perfil-participante-incompleto" data-backdrop="static" tabindex="-1" aria-labelledby="modalPerfilIncompletoLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header" style="background-color: #dc3545;">
+                <h5 class="modal-title" id="modalPerfilIncompletoLabel" style="color: white;">Aviso</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <span id="texto-erro">Esse Discente não atualizou seu perfil no sistema!</span>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Ok</button>
+            </div>
+        </div>
+    </div>
+</div>
