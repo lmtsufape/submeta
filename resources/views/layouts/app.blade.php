117ad68d49f1b9c792e44c383874a7586f4d82f2
caua de SOUZA <160639776+cauaofsouza@users.noreply.github.com>
2026-05-15 01:53:50 -0300

    feat(app.blade): adicionando favicon para todos os clients (#1009)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,6 +93,13 @@
           font-size: 1.2rem;
       }
     </style>
+    {{--favicons and manifest--}}
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
+
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
 </head>
 
 <body>
